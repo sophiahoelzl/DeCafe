53fbcd46746cd21e6ed3cac5812f70f676c578d3
1999ChrisiH <christiane.haider@stud.fh-campuswien.ac.at>
2022-12-14 11:57:48 +0100

    added start button and function that starts the game

--- src/main/java/com/example/decafe/HelloController.java
+++ src/main/java/com/example/decafe/HelloController.java
@@ -1,19 +1,26 @@
 package com.example.decafe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloController {
-    public void startGame(Stage stage) throws IOException {
+    @FXML
+    Button startButton = new Button();
+
+    public void startGame() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         HelloApplication.stage.setTitle("");
         HelloApplication.stage.setScene(scene);
         HelloApplication.stage.show();
     }
+
+
 }
