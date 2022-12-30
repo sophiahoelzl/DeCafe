80440414454ca00f02e710c28d31c75fa88519f4
sophiahoelzl <sophia.hoelzl@stud.fh-campuswien.ac.at>
2022-12-30 13:26:42 +0100

    Customer methods added

--- src/main/java/com/example/decafe/Customer.java
+++ src/main/java/com/example/decafe/Customer.java
@@ -1,6 +1,18 @@
 package com.example.decafe;
 
-import com.example.decafe.enumerations.Products;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 public class Customer {
     private int positionX; // X-Koordinate vom Gast
@@ -8,10 +20,45 @@ public class Customer {
     private String image;  // Bild vom Gast
     private int tableNr; //Tischnummer auf dem der Gast sitzt
     private String order; //Was der Gast bestellt - Kaffee oder Kuchen
+    private List<ImageView> list = new LinkedList<>();
+    private ImageView customerImage = new ImageView();
+
+
+    @FXML
+    public ImageView first;
+    @FXML
+    public ImageView second;
+    @FXML
+    public ImageView third;
+    @FXML
+    public ImageView fourth;
+    @FXML
+    public ImageView fifth;
+    @FXML
+    public ImageView sixth;
+    @FXML
+    public ImageView seventh;
+    @FXML
+    public ImageView eighth;
+
 
     //Getter
     public int getPositionX() {
         return positionX;
+    }
+
+    public List<ImageView> makeListCustomer() {
+
+        list.add(first);
+        list.add(second);
+        list.add(third);
+        list.add(fourth);
+        list.add(fifth);
+        list.add(sixth);
+        list.add(seventh);
+        list.add(eighth);
+
+        return list;
     }
 
     public int getPositionY() {
@@ -27,6 +74,15 @@ public class Customer {
     }
 
     public String getOrder() {
+
+        Random random = new Random();
+        int number = random.nextInt(2);
+
+        switch (number) {
+            case 0 -> order = "Cake";
+            case 1 -> order = "Coffee";
+        }
+
         return order;
     }
 
@@ -51,13 +107,39 @@ public class Customer {
         this.order = order;
     }
 
-    //Gast sucht nach einem Tisch wo er sich hinsetzten, kann
-    public void searchForTable(){
+    public ImageView getRandomPic(List<ImageView> list){
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
+    }
+
+    //Gast sucht nach einem Tisch wo er sich hinsetzten kann
+    public void searchForTable() throws FileNotFoundException {
+
+        list = makeListCustomer();
+        customerImage = getRandomPic(list);
+        displayPerson(customerImage);
+
+        File f = new File("");
+        String filePath;
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            filePath = f.getAbsolutePath().replace("\\", "\\\\") + "\\src\\main\\resources\\com\\example\\decafe\\characterOben.png";;
+        } else {
+            filePath = f.getAbsolutePath().replace("/", "//") + "//src//main//resources//com//example//decafe//characterOben.png";;
+        }
+        InputStream stream = new FileInputStream(filePath);
+        ImageView customerPic = new ImageView(stream);
+        displayPerson(customerPic);
+                
 
     }
 
     //Funktion um Bild von Gast anzuzeigen - vllt auch in HelloController
-    public void displayPerson (String image){
+    public void displayPerson (ImageView image){
+
+        image.setVisible(true);
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(10), image);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(5);
 
     }
 
--- src/main/java/com/example/decafe/HelloApplication.java
+++ src/main/java/com/example/decafe/HelloApplication.java
@@ -18,6 +18,7 @@ public class HelloApplication extends Application {
         stage.setTitle("DeCafé");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
