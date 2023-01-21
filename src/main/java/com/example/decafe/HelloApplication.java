5a1ee13c7f85606309dd7d340c1077293d7ebdb9
1999ChrisiH <christiane.haider@stud.fh-campuswien.ac.at>
2023-01-22 00:31:16 +0100

    added icon to task bar

--- src/main/java/com/example/decafe/HelloApplication.java
+++ src/main/java/com/example/decafe/HelloApplication.java
@@ -3,9 +3,13 @@ package com.example.decafe;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class HelloApplication extends Application {
 
@@ -15,6 +19,11 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         HelloApplication.stage = stage;
+        File f = new File("");
+        String filePath;
+        filePath = f.getAbsolutePath() + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "com" + File.separator + "example" + File.separator + "decafe" + File.separator + "mugTabPic.png";
+        InputStream stream = new FileInputStream(filePath);
+        stage.getIcons().add(new Image(stream));
         stage.setTitle("DeCafé");
         stage.setScene(scene);
         stage.show();
